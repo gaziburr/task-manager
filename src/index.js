e6a995922833fe18c28df1054dcd2f04cc9c5779
gaziburrahman <gaziburrahman1999@gmail.com>
2021-08-03 17:22:04 +0530

    1.different folder created for two models and exported to the other file,//user given data is inserted to the database,second time, by requiring to exported modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
+require('./db/mongoose.js');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())
+const Task = require('./models/task');
+const User = require('./models/user');
+
+app.use(express.json());
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+      console.log(task);
+    })
+    .catch(e => {
+      res.send(e);
+      console.log(e);
+    });
+});
+
 app.post('/users', (req, res) => {
-  res.send('<h1>The server working for client </h1>');
-  console.log(req.body);
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+      console.log(user);
+    })
+    .catch(e => {
+      res.send(e);
+      console.log(e);
+    });
 });
 
 app.listen(port, () => {
