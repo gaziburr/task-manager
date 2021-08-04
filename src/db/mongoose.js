ce8a9790ea3cd98e3da6be4cf369caca00a860f9
gaziburrahman <gaziburrahman1999@gmail.com>
2021-08-04 22:20:55 +0530

    1.I it was not model its module,it was require ,not rre in mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
-// requiring mongoose npm model
-const mongoose = rre('mongoose')
+// requiring mongoose npm module
+const mongoose = require('mongoose')
 //connecting to mongodb database 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
