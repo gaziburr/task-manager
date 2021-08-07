d586dff131852bea7afa79245fc08f6c96d7a2f4
gaziburrahman <gaziburrahman1999@gmail.com>
2021-08-07 14:42:32 +0530

    1.Securely storing password,2.bcryptjs npm module installed,,3. middleware functionality added

--- src/router/user.js
+++ src/router/user.js
@@ -11,7 +11,7 @@ router.post('/users', async (req, res) => {
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
-   console.log(e)
+    console.log(e);
   }
 });
 
@@ -56,18 +56,22 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      // new: true,
+      // runValidators: true,
+    // });
 
+    const user = await User.findById(req.params.id)
+   updates.forEach((update)=>{user[update]=req.body[update]})
+    await user.save();
     if (!user) {
       return res.status(404).send();
     }
-
-    res.send(user);
+console.log(user)
+    res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
+   console.log(e)
   }
 });
 // routes for delete from database
